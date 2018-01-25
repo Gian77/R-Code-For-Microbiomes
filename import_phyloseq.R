41b8bd2f114772381a1577ab7dff86e20e32dfa5
GMNB <gian.benucci@gmail.com>
2018-01-25 16:14:14 -0500

    Update import_phyloseq.R

--- import_phyloseq.R
+++ import_phyloseq.R
@@ -6,15 +6,19 @@ rm(list = ls(all=TRUE))
 library("phyloseq")
 library("Biostrings")
 
-### apprach 1
+# apprach 1)
 biom = import_biom("otu_table.biom")
 map = import_qiime_sample_data("mapping.txt")
 sample_data(biom) <- map
 colnames(tax_table(biom)) = c(k = "Kingdom", p = "Phylum", c = "Class", o = "Order", f = "Family", g = "Genus", s = "Species")
 otus_rep_set <- readDNAStringSet("otus.fasta", format="fasta", seek.first.rec=TRUE, use.names=TRUE)
-biom_ITS <- merge_phyloseq(biom, otus_rep_set)
+physeq_obj <- merge_phyloseq(biom, otus_rep_set)
 
-
+# apprach 2)
+# import OTU table
+otu_tab <- read.delim("otutab_UPARSE_ITS.txt", row.names=1, header=TRUE, sep="\t") 
+head(otu_tab)
+dim(otu_tab)
 
 
 
