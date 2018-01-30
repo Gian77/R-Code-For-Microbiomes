8b4f6af6be9af325cda7d48109374f7c31978649
GMNB <gian.benucci@gmail.com>
2018-01-30 15:29:55 -0500

    Add files via upload

---
+++ filter_taxa.R
@@ -0,0 +1,17 @@
+###########################################################
+# Filter taxa using OTUID across samples
+# in a phyloseq object
+#
+# Usage:	badTaxa = c("OTU_1", "OTU_123", "OTU_456", ...) 
+#			biom_prun = filter_taxa(biom, badTaxa)
+############################################################
+
+
+filter_taxa = function(physeq, badTaxa){
+  allTaxa = taxa_names(physeq)
+  myTaxa <- allTaxa[!(allTaxa %in% badTaxa)]
+  return(prune_taxa(myTaxa, physeq))
+}
+
+
+
