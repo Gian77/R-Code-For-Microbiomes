65f792cdd64eb7cad33f14d68aa741e2d20306d4
GMNB <gian.benucci@gmail.com>
2018-02-04 21:59:26 -0500

    Update filter_taxa.R

--- filter_taxa.R
+++ filter_taxa.R
@@ -1,11 +1,14 @@
-###########################################################
-# Filter taxa using OTUID across samples
-# in a phyloseq object
-#
-# Usage:
-# badTaxa = c("OTU_1", "OTU_123", "OTU_456", ...) 
-#	biom_prun = filter_taxa(biom, badTaxa)
-############################################################
+#######################################################
+#      ***Filter taxa using OTU_ID in Phyloseq***     #
+#                                                     #
+#                  Gian MN Benucci, PhD               #
+#               Michigan State University             #
+#                   benucci@msu.edu                   #      
+#                                                     #
+# Usage:                                              #
+# badTaxa = c("OTU_1", "OTU_123", "OTU_456", ...)     #
+#	biom_prun = filter_taxa(biom, badTaxa)              #
+#######################################################
 
 
 filter_taxa = function(physeq, badTaxa){
