7c0431966df04fe42e5676c68bd3c137209711c1
GMNB <gian.benucci@gmail.com>
2021-03-11 13:36:35 -0500

    Update and rename mean_sd.R to MeanSD.R

---
+++ MeanSD.R
@@ -0,0 +1,17 @@
+# Simple function to calculate the mean and the standard deviation for sample groups
+
+# Example: MeanSD(dataframe, Abund, Crop)
+
+
+MeanSD <- function(data, varname, groupnames){
+  require(plyr)
+  summary_func <- function(x, col){
+    c(mean = mean(x[[col]], na.rm=TRUE),
+      sd = sd(x[[col]], na.rm=TRUE))
+  }
+  data_sum<-ddply(data, groupnames, .fun=summary_func,
+                  varname)
+  data_sum <- rename(data_sum, c("mean" = varname))
+ return(data_sum)
+}
+
